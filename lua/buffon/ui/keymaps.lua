572ad651a6784461026590c6ff612310ada1a408
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-01-20 18:09:11 +0100

    feat(ui): allow to close buffers throught the ui

--- lua/buffon/ui/keymaps.lua
+++ lua/buffon/ui/keymaps.lua
@@ -21,13 +21,35 @@ M.register = function(buffer_id, close)
 	end)
 
 	keymap(buffer_id, "<cr>", function()
-		api.sort_buffers_by_list(vim.api.nvim_buf_get_lines(buffer_id, 0, -1, false))
+		-- sort buffers
+		local buffers = api.get_buffers_list()
+		local lines = vim.api.nvim_buf_get_lines(buffer_id, 0, -1, false)
+		if #lines == #buffers then
+			api.sort_buffers_by_list(lines)
+		end
 
+		-- close unexistent buffers
+		if #lines < #buffers then
+			local group_lines = {}
+			for _, line in ipairs(lines) do
+				group_lines[line] = true
+			end
+
+			for _, buffer in ipairs(buffers) do
+				if group_lines[buffer.name] == nil then
+					vim.api.nvim_buf_delete(buffer.id, { force = false })
+				end
+			end
+		end
+
+		-- open selected buffer
 		local line_num = vim.fn.line(".")
 		local buffer = api.get_buffer_by_index(line_num)
 		if buffer then
 			vim.api.nvim_set_current_buf(buffer.id)
 		end
+
+		-- close modal
 		close()
 	end)
 end
