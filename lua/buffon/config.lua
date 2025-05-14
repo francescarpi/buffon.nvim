5f166de5b7aee7b4e7ef94ab27be686049f5954a
Francesc Arpi Roca <francesc.arpi@gmail.com>
2025-05-14 08:07:21 +0200

    feat: replaces spaces by tabs

--- lua/buffon/config.lua
+++ lua/buffon/config.lua
@@ -1,63 +1,63 @@
 local M = {}
 
 local default = {
-  cyclic_navigation = true,
-  --- possible values:
-  ---   "start": buffers are added at the begginning of the buffers list
-  ---   "end": buffers are added at the end of the list
-  ---   "after": are added after the active buffer
-  new_buffer_position = "end",
-  num_pages = 2,
-  open = {
-    by_default = true,
-    offset = {
-      x = 0,
-      y = 0,
-    },
-    ignore_ft = {
-      "gitcommit",
-      "gitrebase",
-    },
-    default_position = "top_right",
-  },
-  --- Buffers that should be ignored by buffon
-  --- It accepts a list of regex patterns
-  ignore_buff_names = {
-    "diffpanel_",
-  },
-  sort_buffers_by_loaded_status = false,
-  theme = {
-    unloaded_buffer = "#404040",
-    shortcut = "#CC7832",
-    active = "#51afef",
-    unsaved_indicator = "#f70067",
-  },
-  leader_key = ";",
-  mapping_chars = "qweryuiop",
-  keybindings = {
-    goto_next_buffer = "<s-j>",
-    goto_previous_buffer = "<s-k>",
-    move_buffer_up = "<s-l>",
-    move_buffer_down = "<s-h>",
-    move_buffer_top = "<s-t>",
-    move_buffer_bottom = "<s-b>",
-    toggle_buffon_window = "<buffonleader>n",
-    --- Toggle window position allows moving the main window position
-    --- between top-right and bottom-right positions
-    toggle_buffon_window_position = "<buffonleader>nn",
-    switch_previous_used_buffer = "<buffonleader><buffonleader>",
-    close_buffer = "<buffonleader>d",
-    close_buffers_above = "<buffonleader>v",
-    close_buffers_below = "<buffonleader>b",
-    close_all_buffers = "<buffonleader>cc",
-    close_others = "<buffonleader>cd",
-    reopen_recent_closed_buffer = "<buffonleader>t",
-    show_help = "<buffonleader>h",
-    previous_page = "<buffonleader>z",
-    next_page = "<buffonleader>x",
-    move_to_previous_page = "<buffonleader>a",
-    move_to_next_page = "<buffonleader>s",
-  },
+	cyclic_navigation = true,
+	--- possible values:
+	---		"start": buffers are added at the begginning of the buffers list
+	---		"end": buffers are added at the end of the list
+	---		"after": are added after the active buffer
+	new_buffer_position = "end",
+	num_pages = 2,
+	open = {
+		by_default = true,
+		offset = {
+			x = 0,
+			y = 0,
+		},
+		ignore_ft = {
+			"gitcommit",
+			"gitrebase",
+		},
+		default_position = "top_right",
+	},
+	--- Buffers that should be ignored by buffon
+	--- It accepts a list of regex patterns
+	ignore_buff_names = {
+		"diffpanel_",
+	},
+	sort_buffers_by_loaded_status = false,
+	theme = {
+		unloaded_buffer = "#404040",
+		shortcut = "#CC7832",
+		active = "#51afef",
+		unsaved_indicator = "#f70067",
+	},
+	leader_key = ";",
+	mapping_chars = "qweryuiop",
+	keybindings = {
+		goto_next_buffer = "<s-j>",
+		goto_previous_buffer = "<s-k>",
+		move_buffer_up = "<s-l>",
+		move_buffer_down = "<s-h>",
+		move_buffer_top = "<s-t>",
+		move_buffer_bottom = "<s-b>",
+		toggle_buffon_window = "<buffonleader>n",
+		--- Toggle window position allows moving the main window position
+		--- between top-right and bottom-right positions
+		toggle_buffon_window_position = "<buffonleader>nn",
+		switch_previous_used_buffer = "<buffonleader><buffonleader>",
+		close_buffer = "<buffonleader>d",
+		close_buffers_above = "<buffonleader>v",
+		close_buffers_below = "<buffonleader>b",
+		close_all_buffers = "<buffonleader>cc",
+		close_others = "<buffonleader>cd",
+		reopen_recent_closed_buffer = "<buffonleader>t",
+		show_help = "<buffonleader>h",
+		previous_page = "<buffonleader>z",
+		next_page = "<buffonleader>x",
+		move_to_previous_page = "<buffonleader>a",
+		move_to_next_page = "<buffonleader>s",
+	},
 }
 
 ---@class BuffonConfigOpen
@@ -106,33 +106,33 @@ local Config = {}
 
 ---@param opts any
 function Config:new(opts)
-  local cfg = vim.tbl_deep_extend("force", default, opts or {})
-  if cfg.num_pages < 1 or cfg.num_pages > 4 then
-    cfg.num_pages = 1
-  end
-  setmetatable(cfg, self)
-  self.__index = self
+	local cfg = vim.tbl_deep_extend("force", default, opts or {})
+	if cfg.num_pages < 1 or cfg.num_pages > 4 then
+		cfg.num_pages = 1
+	end
+	setmetatable(cfg, self)
+	self.__index = self
 
-  cfg:load_theme()
+	cfg:load_theme()
 
-  return cfg
+	return cfg
 end
 
 ---@param name string
 ---@param color string
 local set_hl = function(name, color)
-  local ns = vim.api.nvim_get_hl_ns({})
-  local hl = vim.api.nvim_get_hl(ns, { name = name })
-  if hl.fg == nil then
-    vim.api.nvim_set_hl(0, name, { fg = color })
-  end
+	local ns = vim.api.nvim_get_hl_ns({})
+	local hl = vim.api.nvim_get_hl(ns, { name = name })
+	if hl.fg == nil then
+		vim.api.nvim_set_hl(0, name, { fg = color })
+	end
 end
 
 function Config:load_theme()
-  set_hl("BuffonUnloadedBuffer", self.theme.unloaded_buffer)
-  set_hl("BuffonShortcut", self.theme.shortcut)
-  set_hl("BuffonLineActive", self.theme.active)
-  set_hl("BuffonUnsavedIndicator", self.theme.unsaved_indicator)
+	set_hl("BuffonUnloadedBuffer", self.theme.unloaded_buffer)
+	set_hl("BuffonShortcut", self.theme.shortcut)
+	set_hl("BuffonLineActive", self.theme.active)
+	set_hl("BuffonUnsavedIndicator", self.theme.unsaved_indicator)
 end
 
 M.Config = Config
--- lua/buffon/storage.lua
+++ lua/buffon/storage.lua
@@ -14,28 +14,28 @@ local Storage = {}
 ---@param workspace string
 ---@param custom_data_path? string
 function Storage:new(workspace, custom_data_path)
-  local o = {
-    workspace = workspace,
-    data_path = custom_data_path or default_data_path,
-  }
-  setmetatable(o, self)
-  self.__index = self
-  return o
+	local o = {
+		workspace = workspace,
+		data_path = custom_data_path or default_data_path,
+	}
+	setmetatable(o, self)
+	self.__index = self
+	return o
 end
 
 --- Converts a string into a URL-friendly "slug" by replacing spaces and non-alphanumeric characters with hyphens.
 ---@param str string The original string to be slugified.
 ---@return string The slugified string.
 local slugify = function(str)
-  local replacement = "-"
-  local result = ""
-  -- loop through each word or number
-  for word in string.gmatch(str, "(%w+)") do
-    result = result .. word .. replacement
-  end
-  -- remove trailing separator
-  result = string.gsub(result, replacement .. "$", "")
-  return result:lower()
+	local replacement = "-"
+	local result = ""
+	-- loop through each word or number
+	for word in string.gmatch(str, "(%w+)") do
+		result = result .. word .. replacement
+	end
+	-- remove trailing separator
+	result = string.gsub(result, replacement .. "$", "")
+	return result:lower()
 end
 
 --- Generates a slugified filename based on the workspace.
@@ -43,87 +43,87 @@ end
 --- The filename is limited to a maximum of 150 characters (excluding the ".json" extension).
 ---@return string The generated filename.
 function Storage:filename()
-  if self.workspace == "/" then
-    return "root.json"
-  end
-  local fn = slugify(self.workspace)
-  if #fn > 150 then
-    fn = fn:sub(1, 150)
-  end
-  return fn .. ".json"
+	if self.workspace == "/" then
+		return "root.json"
+	end
+	local fn = slugify(self.workspace)
+	if #fn > 150 then
+		fn = fn:sub(1, 150)
+	end
+	return fn .. ".json"
 end
 
 --- Generates the full path for the storage file based on the workspace.
 --- The path is constructed using the data path and the slugified filename.
 ---@return string The full path to the storage file.
 function Storage:filename_path()
-  return string.format("%s/%s", self.data_path, self:filename())
+	return string.format("%s/%s", self.data_path, self:filename())
 end
 
 ---@param workspace string
 function Storage:change_workspace(workspace)
-  self.workspace = workspace
+	self.workspace = workspace
 end
 
 ---@param data_path string
 local initialize_data_path = function(data_path)
-  local path = Path:new(data_path)
-  if not path:exists() then
-    path:mkdir()
-  end
+	local path = Path:new(data_path)
+	if not path:exists() then
+		path:mkdir()
+	end
 end
 
 --- Initializes the storage by creating the necessary directory if it does not exist.
 ---@return boolean
 function Storage:init()
-  local ok, msg = pcall(initialize_data_path, self.data_path)
-  if not ok then
-    log.debug("storage couldn't be initialized", msg)
-  end
-  return ok
+	local ok, msg = pcall(initialize_data_path, self.data_path)
+	if not ok then
+		log.debug("storage couldn't be initialized", msg)
+	end
+	return ok
 end
 
 --- Saves the list of buffers to a JSON file.
 --- @param pages_buffers table<table<BuffonBuffer>> The list of buffers to save.
 function Storage:save(pages_buffers)
-  Path:new(self:filename_path()):write(vim.json.encode(pages_buffers), "w")
-  log.debug(#pages_buffers, "buffers saved to disk")
+	Path:new(self:filename_path()):write(vim.json.encode(pages_buffers), "w")
+	log.debug(#pages_buffers, "buffers saved to disk")
 end
 
 --- Loads the list of buffers from a JSON file and returns them.
 ---@param filename_path string
 --- @return table<table<BuffonBuffer>> The list of buffers.
 local load_data = function(filename_path)
-  local path = Path:new(filename_path)
-  if not path:exists() then
-    return {}
-  end
+	local path = Path:new(filename_path)
+	if not path:exists() then
+		return {}
+	end
 
-  local data = path:read()
-  local buffers = vim.json.decode(data)
+	local data = path:read()
+	local buffers = vim.json.decode(data)
 
-  -- Buffer IDs must be set to nil because, by default, all buffers will be loaded
-  -- within the buffer list but not in Neovim.
-  for _, page in ipairs(buffers) do
-    for _, buffer in ipairs(page) do
-      buffer.id = nil
-    end
-  end
+	-- Buffer IDs must be set to nil because, by default, all buffers will be loaded
+	-- within the buffer list but not in Neovim.
+	for _, page in ipairs(buffers) do
+		for _, buffer in ipairs(page) do
+			buffer.id = nil
+		end
+	end
 
-  return buffers
+	return buffers
 end
 
 --- Loads the list of buffers from a JSON file and returns them.
 --- @return table<table<BuffonBuffer>> The list of buffers.
 function Storage:load()
-  local ok, pages_buffers = pcall(load_data, self:filename_path())
-  if not ok then
-    error("Buffers couldn't be loaded from disk")
-    return {}
-  end
+	local ok, pages_buffers = pcall(load_data, self:filename_path())
+	if not ok then
+		error("Buffers couldn't be loaded from disk")
+		return {}
+	end
 
-  log.debug(#pages_buffers, "buffers loaded from disk")
-  return pages_buffers
+	log.debug(#pages_buffers, "buffers loaded from disk")
+	return pages_buffers
 end
 
 M.Storage = Storage
